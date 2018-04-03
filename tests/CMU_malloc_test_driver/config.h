3596abfc5bb39f532185ddee3c9d3073935e70a2
error: no name <error: no email>
2018-04-03 19:24:28 -0400

    Refactor: makes sure test Makefile 'clean' directive removes tlsf.o from main directory

--- tests/CMU_malloc_test_driver/config.h
+++ tests/CMU_malloc_test_driver/config.h
@@ -27,12 +27,8 @@
   "cp-decl-bal.rep",\
   "expr-bal.rep",\
   "coalescing-bal.rep",\
-  "random-bal.rep",\
-  "random2-bal.rep",\
   "binary-bal.rep",\
-  "binary2-bal.rep",\
-  "realloc-bal.rep",\
-  "realloc2-bal.rep"
+  "binary2-bal.rep"
 
 /*
  * This constant gives the estimated performance of the libc malloc
